c9c294011e5fb1ace8905cfde9c1f857be7c19c4
Jameson Vallar <jvallar@gmail.com>
2018-02-08 07:51:08 +0800

    Remove sequence

--- examples/index.js
+++ examples/index.js
@@ -20,9 +20,9 @@ ssp.init(function () {
     ssp.on('ready', function () {
         console.log("Device is ready");
         ssp.enable();
-        ssp.commands.sync().get_denomination_level(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
-        ssp.commands.sync().enable_payout_device()
-        ssp.commands.sync().payout_amount(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44, 0x58)
+        ssp.commands.get_denomination_level(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
+        // ssp.commands.sync().enable_payout_device()
+        // ssp.commands.sync().payout_amount(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44, 0x58)
         // ssp.commands.sync().smart_empty();
         // ssp.commands.sync().cashbox_payout_operation_data();
 
