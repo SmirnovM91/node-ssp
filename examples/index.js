cdab9e891cc5aa5a93aad4c2f54bf8b420a37e4a
Jameson Vallar <jvallar@gmail.com>
2018-02-08 08:23:01 +0800

    Added payout amount

--- examples/index.js
+++ examples/index.js
@@ -20,7 +20,7 @@ ssp.init(function () {
     ssp.on('ready', function () {
         console.log("Device is ready");
         ssp.enable();
-        ssp.commands.enable_payout_device()
+        ssp.commands.sync().enable_payout_device()
         ssp.commands.get_denomination_level(0xC8, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
         ssp.commands.payout_amount(0xC8, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44, 0x58)
         // ssp.commands.sync().smart_empty();
