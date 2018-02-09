853596071e2535494fd89e28b38dd2ba4651aec9
Jameson Vallar <jvallar@gmail.com>
2018-02-09 23:30:06 +0800

    encryptions

--- index.js
+++ index.js
@@ -66,7 +66,10 @@ var SSPInstance = Class.extend({
     createHostEncryptionKeys: function (data) {
         var commands = this.commands, self = this;
         data.shift()
+        console.log(data)
         var hexString = convertHex.bytesToHex(data);
+        console.log(hexString)
+
         var slaveIntKey = bigInt(hexString, 16);
         var slaveIntKeyString = ""
         if (!slaveIntKey.isSmall) {
