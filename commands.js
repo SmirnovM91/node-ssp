4e1c2fea036c11f0b2552e3ea1ca00a93167e33e
Jameson Vallar <jvallar@gmail.com>
2018-02-11 12:30:25 +0800

    encryptions

--- commands.js
+++ commands.js
@@ -90,7 +90,7 @@ var Commands = Class.extend({
                 var data = this.byteToHexString(eCommandLine)
                 console.log(data)
                 console.log(self.keys.variableKey, self.keys.fixedKey)
-                var keys = (self.keys.variableKey ) | (self.keys.fixedKey );
+                var keys = this.byteToHexString(self.keys.variableKey).join() | this.byteToHexString(self.keys.fixedKey).join
                 console.log(keys);
                 var encryptedData = CryptoJS.AES.encrypt(data, keys);
                 var encryptedString = encryptedData.toString()
