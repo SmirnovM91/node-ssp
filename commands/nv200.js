459977be3a41b3634366e4fd059ec969b40926ef
Jameson Vallar <jvallar@gmail.com>
2018-02-13 15:25:41 +0800

    encryptions

--- commands/nv200.js
+++ commands/nv200.js
@@ -26,6 +26,7 @@ module.exports = {
     "get_firmware_version": 0x20,
     "get_dataset_version": 0x21,
     "set_generator": function () {
+        this.sequence = 0x00;
         return 0x4a;
     },
     "set_modulus": function () {
@@ -33,6 +34,7 @@ module.exports = {
         return 0x4b;
     },
     "request_key_exchange": function () {
+        this.sequence = 0x00;
         return 0x4c;
     },
     "poll_with_ack": 0x56,
