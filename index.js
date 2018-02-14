d256ca074cd203088ebac6a2dee56c8dc220ebc7
Jameson Vallar <jvallar@gmail.com>
2018-02-14 10:07:26 +0800

    encryptions

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ var SSPInstance = Class.extend({
         commands.set_modulus.apply(this, modulusArray)
         commands.host_set_modulus.apply(this, modulusArray)
         commands.request_key_exchange.apply(this, hostIntArray)
-        commands.host_request_key_exchange.apply(this, hostIntArray)
+        // commands.host_request_key_exchange.apply(this, hostIntArray)
     },
     createHostEncryptionKeys: function (data) {
         var commands = this.commands, self = this;
