932b14b059767433603cb07196d7246274339b27
Jameson Vallar <jvallar@gmail.com>
2018-02-14 23:30:46 +0800

    encryptions

--- commands.js
+++ commands.js
@@ -94,7 +94,7 @@ var Commands = Class.extend({
                     }
                     return a;
                 }
-                var key = parse(Array.prototype.slice.call(self.keys.key, 0), 8).concat(parse(Array.prototype.slice.call(self.keys.fixedKey, 0).reverse(), 8))
+                var key = parse(Array.prototype.slice.call(self.keys.fixedKey, 0), 8).concat(parse(Array.prototype.slice.call(self.keys.key, 0).reverse(), 8))
 
                 var iv = [ 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,35, 36 ];
                 console.log(key)
