35c333052e29a8e138a47b16f88e25e881aebd7a
Jameson Vallar <jvallar@gmail.com>
2018-02-16 21:56:08 +0800

    encryptions

--- index.js
+++ index.js
@@ -107,8 +107,6 @@ var SSPInstance = Class.extend({
             self.keys.variableKey = self.keys.key
             self.keys.finishEncryption = true
             commands.setKeys(self.keys)
-            self.emit("ready");
-            console.log("ready")
         }
     },
     enable: function (cb) {
@@ -490,7 +488,7 @@ var SSPInstance = Class.extend({
                         } else {
                             commands.exec(function () {
                                 cb && cb();
-                                self.emit("ready");
+                                // self.emit("ready");
                             });
                         }
                     }, 2000);
