93d6b2a1a93c9144427e257fc5b45434f5add3ba
Jameson Vallar <jvallar@gmail.com>
2018-02-16 21:31:41 +0800

    encryptions

--- examples/index.js
+++ examples/index.js
@@ -19,12 +19,12 @@ ssp.init(function () {
     ssp.on('ready', function () {
         console.log("Device is ready");
         setTimeout(function () {
-            // ssp.commands.setup_request()
+            ssp.commands.setup_request()
             ssp.commands.set_denomination_route(0x00, 0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
             // ssp.commands.enable_payout_device()
             // ssp.enable();
             // ssp.commands.get_denomination_level(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
-        }, 3000);
+        }, 1000);
 
         // ssp.commands.get_denomination_route(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
         // ssp.commands.get_denomination_level(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
