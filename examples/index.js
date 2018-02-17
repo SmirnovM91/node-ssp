fa0bd70756387d7ede06ff5c91e90977018c4cc0
Jameson Vallar <jvallar@gmail.com>
2018-02-17 22:07:06 +0800

    encryptions

--- examples/index.js
+++ examples/index.js
@@ -9,93 +9,87 @@ var notes = {
     5: "50USD",
     6: "100USD"
 };
-var esspInstance = new essp()
-esspInstance.initialize({
-    device: 'COM1', //device address
-    type: "nv200", //device type
-    currencies: [1, 1, 1, 1, 1, 1] //currencies types acceptable. Here all but 100USD
-})
-esspInstance.on("event", function (data) {
-    console.log(data)
-})
-esspInstance.on("error", function (data) {
-    console.log(data)
-})
-setTimeout(()=> {
-    esspInstance.initiateKeys()
-}, 1000)
-
-
-//
-// ssp = new ssp({
+// var esspInstance = new essp()
+// esspInstance.initialize({
 //     device: 'COM1', //device address
 //     type: "nv200", //device type
 //     currencies: [1, 1, 1, 1, 1, 1] //currencies types acceptable. Here all but 100USD
-// });
+// })
+// setTimeout(()=> {
+//     esspInstance.initiateKeys()
+// }, 1000)
+
+
 //
-// ssp.init(function () {
-//     console.log("init")
-//     ssp.on('ready', function () {
-//         console.log("Device is ready");
-//         setTimeout(function () {
-//             // ssp.commands.set_denomination_route(0x00, 0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
-//             // ssp.commands.enable_payout_device()
-//             // ssp.enable();
-//             // ssp.commands.get_denomination_level(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
-//         }, 5000);
-//
-//         // ssp.commands.get_denomination_route(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
-//         // ssp.commands.get_denomination_level(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
-//         // ssp.commands.payout_amount(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44, 0x58)
-//         // ssp.commands.sync().smart_empty();
-//         // ssp.commands.sync().cashbox_payout_operation_data();
-//
-//     });
-//     ssp.on('read_note', function (note) {
-//         if (note > 0) {
-//             console.log("GOT", notes[note]);
-//             if (note === 3) {
-//                 // suddenly we decided that we don't need 1000 KZT
-//                 ssp.commands.exec("reject_banknote");
-//             }
-//         }
-//     });
-//     ssp.on('disabled', function () {
-//         console.log("disabled");
-//     });
-//     ssp.on('note_cleared_from_front', function (note) {
-//         console.log("note_cleared_from_front");
-//     });
-//     ssp.on('note_cleared_to_cashbox', function (note) {
-//         console.log("note_cleared_to_cashbox");
-//     });
-//     ssp.on('credit_note', function (note) {
-//         console.log("CREDIT", notes[note]);
-//     });
-//     ssp.on("safe_note_jam", function (note) {
-//         console.log("Jammed", note);
-//         //TODO: some notifiaction, recording, etc.
-//     });
-//     ssp.on("unsafe_note_jam", function (note) {
-//         console.log("Jammed inside", note);
-//         //TODO: some notifiaction, recording, etc.
-//     });
-//     ssp.on("fraud_attempt", function (note) {
-//         console.log("Fraud!", note);
-//         //TODO: some notifiaction, recording, etc.
-//     });
-//     ssp.on("stacker_full", function (note) {
-//         console.log("I'm full, do something!");
-//         ssp.disable();
-//         //TODO: some notifiaction, recording, etc.
-//     });
-//     ssp.on("note_rejected", function (reason) {
-//         console.log("Rejected!", reason);
-//     });
-//     ssp.on("error", function (err) {
-//         console.log(err.code, err.message);
-//     });
-// });
+ssp = new ssp({
+    device: 'COM1', //device address
+    type: "nv200", //device type
+    currencies: [1, 1, 1, 1, 1, 1] //currencies types acceptable. Here all but 100USD
+});
+
+ssp.init(function () {
+    console.log("init")
+    ssp.on('ready', function () {
+        console.log("Device is ready");
+        setTimeout(function () {
+            // ssp.commands.set_denomination_route(0x00, 0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
+            // ssp.commands.enable_payout_device()
+            // ssp.enable();
+            // ssp.commands.get_denomination_level(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
+        }, 5000);
+
+        // ssp.commands.get_denomination_route(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
+        // ssp.commands.get_denomination_level(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44)
+        // ssp.commands.payout_amount(0x64, 0x00, 0x00, 0x00, 0x55, 0x53, 0x44, 0x58)
+        // ssp.commands.sync().smart_empty();
+        // ssp.commands.sync().cashbox_payout_operation_data();
+
+    });
+    ssp.on('read_note', function (note) {
+        if (note > 0) {
+            console.log("GOT", notes[note]);
+            if (note === 3) {
+                // suddenly we decided that we don't need 1000 KZT
+                ssp.commands.exec("reject_banknote");
+            }
+        }
+    });
+    ssp.on('disabled', function () {
+        console.log("disabled");
+    });
+    ssp.on('note_cleared_from_front', function (note) {
+        console.log("note_cleared_from_front");
+    });
+    ssp.on('note_cleared_to_cashbox', function (note) {
+        console.log("note_cleared_to_cashbox");
+    });
+    ssp.on('credit_note', function (note) {
+        console.log("CREDIT", notes[note]);
+    });
+    ssp.on("safe_note_jam", function (note) {
+        console.log("Jammed", note);
+        //TODO: some notifiaction, recording, etc.
+    });
+    ssp.on("unsafe_note_jam", function (note) {
+        console.log("Jammed inside", note);
+        //TODO: some notifiaction, recording, etc.
+    });
+    ssp.on("fraud_attempt", function (note) {
+        console.log("Fraud!", note);
+        //TODO: some notifiaction, recording, etc.
+    });
+    ssp.on("stacker_full", function (note) {
+        console.log("I'm full, do something!");
+        ssp.disable();
+        //TODO: some notifiaction, recording, etc.
+    });
+    ssp.on("note_rejected", function (reason) {
+        console.log("Rejected!", reason);
+    });
+    ssp.on("error", function (err) {
+        console.log(err.code, err.message);
+    });
+});
 
 process.on('SIGINT', function () {
     process.exit(0);
