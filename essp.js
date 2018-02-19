692ec7370eb99ed1c85d76f099e34c9ef19c7f10
Jameson Vallar <jvallar@gmail.com>
2018-02-19 11:18:14 +0800

    encryptions

--- essp.js
+++ essp.js
@@ -120,6 +120,7 @@ export default class eSSP extends EventEmitter {
 
         console.log(this.keys)
         let data = await this.sync()
+        let data = await this.sync()
         data = await this.sendGenerator()
         data = await this.sendGenerator()
         data = await this.sendModulus()
