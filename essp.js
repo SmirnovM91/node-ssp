13594a0d622830032c03d868c1ebd46a94bcff24
Jameson Vallar <jvallar@gmail.com>
2018-02-21 13:51:29 +0800

    encryptions

--- essp.js
+++ essp.js
@@ -370,7 +370,6 @@ export default class eSSP extends EventEmitter {
 
             var key = parse(Array.prototype.slice.call(this.keys.fixedKey, 0), 8).concat(this.parseHexString(this.keys.key.toString(16), 8))
 
-            console.log("ENCRYPTIONS KEY = ", key)
             var aesCtr = new aesjs.AES(key);
             var uint8Array = aesCtr.encrypt(eCommandLine);
             var eDATA = [].slice.call(uint8Array)
